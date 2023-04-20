4593f587d8e691923388f90cd3601d2991c3b751
Ana Silvera <anasilverx@gmail.com>
2023-04-20 12:02:31 -0700

    Created Planet class and endpoint to list planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, moon):
+        self.id = id #generate randmon id 
+        self.name = name
+        self.description = description
+        self.moon = moon 
+
+planets = [
+    Planet(3, "Earth", "Third planet from sun", ["Moon"]),
+    Planet(5, "Jupiter", "The largest in the solar system. Gas giant", ["Europa", "Io", "Elara"]),
+    Planet(4, "Mars", "Third largest. Red Planet", ["Phobos", "Deimos"])
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+@planets_bp.route("", methods=["GET"])
+def handle_books():
+    planets_response = [vars(planets) for planet in planets]
+    return jsonify(planets_response), 200
